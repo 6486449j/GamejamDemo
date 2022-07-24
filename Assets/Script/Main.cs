57fc2a5252ddb7593e141b412d9469bac9a835da
6486449j <1966063360@qq.com>
2022-07-24 19:23:07 +0800

    init

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cam;
+    public BoxCollider2D de;
+    //public BoxCollider2D thus;
+
+    Vector3 v2s;
+    Vector3 v2r;
+    Vector3 thisv;
+
+    void Start()
+    {
+        v2s = de.transform.position;
+        //v2r = thus.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            portal1();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            portal2();
+        }
+    }
+
+    public void portal1()
+    {
+        Vector3 v3c = new Vector3(this.transform.position.x, this.transform.position.y, 30);   
+        this.transform.position = v3c;
+        Vector3 v3c1 = new Vector3(this.transform.position.x,0, 15);
+        cam.transform.position = v3c1;
+    }
+    public void portal2()
+    {
+        Vector3 v3c = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        this.transform.position = v3c;
+        Vector3 v3c1 = new Vector3(this.transform.position.x, 0, -10);
+        cam.transform.position = v3c1;
+    }
+}
---
+++ Assets/Script/Preview.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preview : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /*
+     * 
+    1. 游戏类型 ： 2d横版 解密过关游戏
+        2. 游戏设定 ：
+        2.1 背景 ： 探索废弃（？）
+        2.2 流程 ： 大致分为 4部分 初入》接触真相》跌入谷底》抵达真相
+        2.3 玩法 ： 游戏世界分为两个 一个表世界，一个里世界 可以通过道具进行切换 ，
+                    也可通过游戏进行的流程进行固定切换。（类似寂静岭或homesick）
+                    游戏的解密需要在两个世界之间来回切换完成。
+        2.4 素材 ： 像素类型
+
+    */
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
